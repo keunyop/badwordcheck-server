a622fd1c303b10f36978bcbb3ad7aed011088017
keunyop <kylee1112@hotmail.com>
2019-11-29 21:20:01 +0900

    developing

--- badwordcheck/src/main/java/com/badwordcheck/webservice/WebRestController.java
+++ badwordcheck/src/main/java/com/badwordcheck/webservice/WebRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebRestController {
 	@GetMapping("/hello")
-	public String hello() {
-		return "HelloWorld";
-	}
+    public String hello() {
+        return "HelloWorld";
+    }
 }
