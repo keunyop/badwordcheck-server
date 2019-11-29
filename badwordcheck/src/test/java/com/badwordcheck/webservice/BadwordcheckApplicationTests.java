453f0337d76e6f5c9e8124be67d773c5f1f82b5c
keunyop <kylee1112@hotmail.com>
2019-11-29 21:05:30 +0900

    Initial commit

---
+++ badwordcheck/src/test/java/com/badwordcheck/webservice/BadwordcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badwordcheck.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadwordcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
